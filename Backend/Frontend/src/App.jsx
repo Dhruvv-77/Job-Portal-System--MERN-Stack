e7cf75480a3e52367cfd3ae10a100134ec40d63b
Dhruvv-77 <dhruvchandarana4320@gmail.com>
2025-07-02 14:00:39 +0530

    xx

--- Backend/Frontend/src/App.jsx
+++ Backend/Frontend/src/App.jsx
@@ -114,7 +114,7 @@ const appRouter = createBrowserRouter([
 function App() {
   return (
     <div>
-      
+      <Navbar />
       <RouterProvider router={appRouter}></RouterProvider>
     </div>
   );
