5c71d2427636c7108936b93b22832f63a16c7a9d
Dhruvv-77 <dhruvchandarana4320@gmail.com>
2025-07-02 13:46:30 +0530

    update link

--- Backend/index.js
+++ Backend/index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: ["https://job-portal-system-mern-stack-1.onrender.com"],
+  origin: ["http://localhost:5173"],
   credentials: true,
 };
 
